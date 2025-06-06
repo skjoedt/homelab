6b64843094c83abc803353e68af1e2b6f3187a69
Christian Skjødt <skjoedt@gmail.com>
2025-06-06 20:57:02 +0200

    Refactor and add monitoring (#16)
    * remove flux
    
    * add monitoring
    
    ---------
    
    Co-authored-by: Christian Skjødt Steenmans <csh@evaxion-biotech.com>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"] (system:
       let
         pkgs = import nixpkgs {
         inherit system;
